191a678c55ebab76d9ca4ca27d71af54b32f0bca
mathiasooi <mathias7142@gmail.com>
2021-08-15 16:02:09 -0700

    LoginViewController Documentation

--- Test3/ViewControllers/LoginViewController.swift
+++ Test3/ViewControllers/LoginViewController.swift
@@ -16,61 +16,102 @@ class LoginViewController: UIViewController {
     @IBOutlet var password: UITextField!
     @IBOutlet weak var message: UILabel!
     
-    @IBAction func login(_ sender: UIButton) {
-        guard let email = email.text, !email.isEmpty else {
-            setMessage(msg: "Email or Password field cannot be empty")
-            return
-        }
-        guard let password = password.text, !password.isEmpty else {
-            setMessage(msg: "Email or Password field cannot be empty")
-            return
-        }
-        
-        setMessage(msg: "")
-        
-        print("Email:", email)
-        print("Password:", password)
-        print("login pressed")
-        
-        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
-            guard let user = authResult?.user, error == nil else {
-                self!.setMessage(msg: error!.localizedDescription)
-                print(error!.localizedDescription)
-                return
-            }
-            self!.setMessage(msg: "\(user.email!) logged in")
-            print("logged in", user.email!)
-        }
+    /**
+    Custom error for reading logins
+     */
+    enum LoginError: Error {
+        case emptyField
     }
     
-    @IBAction func signUp(_ sender: UIButton) {
+    /**
+     Reads data from email and password textfield
+     
+     - Throws: `LoginError.emptyField`
+     
+     - Returns: Tuple of email and Data
+     
+     */
+    func readData() throws -> (email: String, password: String)? {
         guard let email = email.text, !email.isEmpty else {
-            setMessage(msg: "Email or Password field cannot be empty")
-            return
+            throw LoginError.emptyField
         }
         guard let password = password.text, !password.isEmpty else {
-            setMessage(msg: "Email or Password field cannot be empty")
-            return
+            throw LoginError.emptyField
         }
-        
-        setMessage(msg: "")
-        
+                
         print("Email:", email)
         print("Password:", password)
-        print("signup pressed")
         
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            guard let user = authResult?.user, error == nil else {
-                self.setMessage(msg: error!.localizedDescription)
-                print(error!.localizedDescription)
-                return
-            }
-            self.setMessage(msg: "\(user.email!) created")
-            print("\(user.email!) created")
-        }
+        return (email, password)
     }
-    
+
+    /**
+     Sets UILabel to given message
+     
+     - Parameters:
+        - msg: Message you want displayed
+     */
     func setMessage(msg: String) {
         message.text = msg
+    }
+    
+    /**
+     Handles authentification of  user using FIrebase, when login button pressed
+     */
+    @IBAction func login(_ sender: UIButton) {
+        // print("login pressed")
+        do {
+            setMessage(msg: "")
+            let (email, password) = try readData()!
+            
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                guard let user = authResult?.user, error == nil else {
+                    // Handle Firebase auth errors
+                    self!.setMessage(msg: error!.localizedDescription)
+                    print(error!.localizedDescription)
+                    return
+                }
+                self!.setMessage(msg: "\(user.email!) logged in")
+                print("logged in", user.email!)
+            }
+        }
+        catch LoginError.emptyField {
+            // Handle custom invalid empty error
+            setMessage(msg: "Email or Password field cannot be empty")
+        }
+        catch {
+            // Handle other errors
+            return
+        }
+    }
+    
+    /**
+     Handles signing up users with Firebase, when sign up button pressed
+     */
+    @IBAction func signUp(_ sender: UIButton) {
+        // print("sign up pressed")
+        do {
+            setMessage(msg: "")
+            let (email, password) = try readData()!
+            
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                guard let user = authResult?.user, error == nil else {
+                    // Handle Firebase auth errors
+                    self.setMessage(msg: error!.localizedDescription)
+                    print(error!.localizedDescription)
+                    return
+                }
+                self.setMessage(msg: "\(user.email!) created")
+                print("\(user.email!) created")
+            }
+        }
+        catch LoginError.emptyField {
+            // Handle custom invalid empty error
+            setMessage(msg: "Email or Password field cannot be empty")
+        }
+        catch {
+            // Handle other errors
+            return
+        }
     }
 }
